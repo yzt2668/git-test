a12acfb88e44bac837ab1569c4d26a6cd06e5cf1
tzzach <2668432188@qq.com>
2022-01-20 11:46:44 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author TzZach
+ * @since 2022-01-20 11:37
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
